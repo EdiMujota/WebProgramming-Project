5113aa1c730f4d6ed5e77cd7f86c21166032c4b8
EdiMujota <93375392+EdiMujota@users.noreply.github.com>
2023-02-05 22:32:08 +0100

    Adding Migrations in the application

---
+++ Migrations/202301091415208_ELibraryDB.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApplication_LibraryManagementProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ELibraryDB : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
